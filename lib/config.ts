2b3fff3deb9cb69d53baa3074bd2dc8a83992796
Shramee Srivastav <shramee.srivastav@gmail.com>
2026-03-14 18:40:51 +0530

    chore: update contract abi

--- lib/config.ts
+++ lib/config.ts
@@ -12,128 +12,329 @@ export const AVNU_BASE_URL = "https://starknet.api.avnu.fi";
 // deposit(commitment: felt252) locks funds behind a secret commitment
 export const HIDEMI_ABI = [
   {
-    type: "interface",
-    name: "hidemi::IHidemi",
+    type: 'impl',
+    name: 'UpgradeableImpl',
+    interface_name: 'openzeppelin_upgrades::interface::IUpgradeable',
+  },
+  {
+    type: 'interface',
+    name: 'openzeppelin_upgrades::interface::IUpgradeable',
     items: [
       {
-        type: "function",
-        name: "deposit",
+        type: 'function',
+        name: 'upgrade',
         inputs: [
           {
-            name: "commitment",
-            type: "core::felt252",
+            name: 'new_class_hash',
+            type: 'core::starknet::class_hash::ClassHash',
           },
         ],
         outputs: [],
-        state_mutability: "external",
+        state_mutability: 'external',
+      },
+    ],
+  },
+  {
+    type: 'impl',
+    name: 'HidemiImpl',
+    interface_name: 'hidemi::IHidemi',
+  },
+  {
+    type: 'struct',
+    name: 'core::integer::u256',
+    members: [
+      {
+        name: 'low',
+        type: 'core::integer::u128',
       },
       {
-        type: "function",
-        name: "withdraw",
+        name: 'high',
+        type: 'core::integer::u128',
+      },
+    ],
+  },
+  {
+    type: 'struct',
+    name: 'hidemi::chamber::Asset',
+    members: [
+      {
+        name: 'amount',
+        type: 'core::integer::u256',
+      },
+      {
+        name: 'addr',
+        type: 'core::starknet::contract_address::ContractAddress',
+      },
+    ],
+  },
+  {
+    type: 'struct',
+    name: 'core::byte_array::ByteArray',
+    members: [
+      {
+        name: 'data',
+        type: 'core::array::Array::<core::bytes_31::bytes31>',
+      },
+      {
+        name: 'pending_word',
+        type: 'core::felt252',
+      },
+      {
+        name: 'pending_word_len',
+        type: 'core::integer::u32',
+      },
+    ],
+  },
+  {
+    type: 'interface',
+    name: 'hidemi::IHidemi',
+    items: [
+      {
+        type: 'function',
+        name: 'deposit',
         inputs: [
           {
-            name: "recipient",
-            type: "core::starknet::ContractAddress",
+            name: 'hash',
+            type: 'core::integer::u256',
           },
           {
-            name: "nullifier_hash",
-            type: "core::felt252",
-          },
-          {
-            name: "root",
-            type: "core::felt252",
+            name: 'asset',
+            type: 'hidemi::chamber::Asset',
           },
         ],
         outputs: [],
-        state_mutability: "external",
+        state_mutability: 'external',
       },
       {
-        type: "function",
-        name: "is_known_root",
+        type: 'function',
+        name: 'cctp_deposit',
         inputs: [
           {
-            name: "root",
-            type: "core::felt252",
+            name: 'message',
+            type: 'core::byte_array::ByteArray',
           },
-        ],
-        outputs: [
           {
-            type: "core::bool",
+            name: 'attestation',
+            type: 'core::byte_array::ByteArray',
           },
         ],
-        state_mutability: "view",
+        outputs: [],
+        state_mutability: 'external',
       },
       {
-        type: "function",
-        name: "get_denomination",
+        type: 'function',
+        name: 'clear_fees',
+        inputs: [],
+        outputs: [],
+        state_mutability: 'external',
+      },
+    ],
+  },
+  {
+    type: 'impl',
+    name: 'OwnableMixinImpl',
+    interface_name: 'openzeppelin_access::ownable::interface::OwnableABI',
+  },
+  {
+    type: 'interface',
+    name: 'openzeppelin_access::ownable::interface::OwnableABI',
+    items: [
+      {
+        type: 'function',
+        name: 'owner',
         inputs: [],
         outputs: [
           {
-            type: "core::felt252",
+            type: 'core::starknet::contract_address::ContractAddress',
           },
         ],
-        state_mutability: "view",
+        state_mutability: 'view',
+      },
+      {
+        type: 'function',
+        name: 'transfer_ownership',
+        inputs: [
+          {
+            name: 'new_owner',
+            type: 'core::starknet::contract_address::ContractAddress',
+          },
+        ],
+        outputs: [],
+        state_mutability: 'external',
+      },
+      {
+        type: 'function',
+        name: 'renounce_ownership',
+        inputs: [],
+        outputs: [],
+        state_mutability: 'external',
+      },
+      {
+        type: 'function',
+        name: 'transferOwnership',
+        inputs: [
+          {
+            name: 'newOwner',
+            type: 'core::starknet::contract_address::ContractAddress',
+          },
+        ],
+        outputs: [],
+        state_mutability: 'external',
+      },
+      {
+        type: 'function',
+        name: 'renounceOwnership',
+        inputs: [],
+        outputs: [],
+        state_mutability: 'external',
       },
     ],
   },
   {
-    type: "impl",
-    name: "HidemiImpl",
-    interface_name: "hidemi::IHidemi",
-  },
-  {
-    type: "event",
-    name: "hidemi::Hidemi::Deposit",
-    kind: "struct",
+    type: 'struct',
+    name: 'interfaces::message_transmitter_v2::IMessageTransmitterV2Dispatcher',
     members: [
       {
-        name: "commitment",
-        type: "core::felt252",
-        kind: "key",
-      },
-      {
-        name: "leaf_index",
-        type: "core::integer::u128",
-        kind: "data",
-      },
-      {
-        name: "timestamp",
-        type: "core::integer::u64",
-        kind: "data",
+        name: 'contract_address',
+        type: 'core::starknet::contract_address::ContractAddress',
       },
     ],
   },
   {
-    type: "event",
-    name: "hidemi::Hidemi::Withdrawal",
-    kind: "struct",
+    type: 'struct',
+    name: 'openzeppelin_token::erc20::interface::IERC20Dispatcher',
     members: [
       {
-        name: "recipient",
-        type: "core::starknet::ContractAddress",
-        kind: "key",
-      },
-      {
-        name: "nullifier_hash",
-        type: "core::felt252",
-        kind: "key",
+        name: 'contract_address',
+        type: 'core::starknet::contract_address::ContractAddress',
       },
     ],
   },
   {
-    type: "event",
-    name: "hidemi::Hidemi::Event",
-    kind: "enum",
+    type: 'struct',
+    name: 'hidemi::chamber::IChamberDispatcher',
+    members: [
+      {
+        name: 'contract_address',
+        type: 'core::starknet::contract_address::ContractAddress',
+      },
+    ],
+  },
+  {
+    type: 'constructor',
+    name: 'constructor',
+    inputs: [
+      {
+        name: 'message_transmitter',
+        type: 'interfaces::message_transmitter_v2::IMessageTransmitterV2Dispatcher',
+      },
+      {
+        name: 'fee_recipient',
+        type: 'core::starknet::contract_address::ContractAddress',
+      },
+      {
+        name: 'owner',
+        type: 'core::starknet::contract_address::ContractAddress',
+      },
+      {
+        name: 'usdc',
+        type: 'openzeppelin_token::erc20::interface::IERC20Dispatcher',
+      },
+      {
+        name: 'chamber',
+        type: 'hidemi::chamber::IChamberDispatcher',
+      },
+    ],
+  },
+  {
+    type: 'event',
+    name: 'openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferred',
+    kind: 'struct',
+    members: [
+      {
+        name: 'previous_owner',
+        type: 'core::starknet::contract_address::ContractAddress',
+        kind: 'key',
+      },
+      {
+        name: 'new_owner',
+        type: 'core::starknet::contract_address::ContractAddress',
+        kind: 'key',
+      },
+    ],
+  },
+  {
+    type: 'event',
+    name: 'openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferStarted',
+    kind: 'struct',
+    members: [
+      {
+        name: 'previous_owner',
+        type: 'core::starknet::contract_address::ContractAddress',
+        kind: 'key',
+      },
+      {
+        name: 'new_owner',
+        type: 'core::starknet::contract_address::ContractAddress',
+        kind: 'key',
+      },
+    ],
+  },
+  {
+    type: 'event',
+    name: 'openzeppelin_access::ownable::ownable::OwnableComponent::Event',
+    kind: 'enum',
     variants: [
       {
-        name: "Deposit",
-        type: "hidemi::Hidemi::Deposit",
-        kind: "nested",
+        name: 'OwnershipTransferred',
+        type: 'openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferred',
+        kind: 'nested',
       },
       {
-        name: "Withdrawal",
-        type: "hidemi::Hidemi::Withdrawal",
-        kind: "nested",
+        name: 'OwnershipTransferStarted',
+        type: 'openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferStarted',
+        kind: 'nested',
+      },
+    ],
+  },
+  {
+    type: 'event',
+    name: 'openzeppelin_upgrades::upgradeable::UpgradeableComponent::Upgraded',
+    kind: 'struct',
+    members: [
+      {
+        name: 'class_hash',
+        type: 'core::starknet::class_hash::ClassHash',
+        kind: 'data',
+      },
+    ],
+  },
+  {
+    type: 'event',
+    name: 'openzeppelin_upgrades::upgradeable::UpgradeableComponent::Event',
+    kind: 'enum',
+    variants: [
+      {
+        name: 'Upgraded',
+        type: 'openzeppelin_upgrades::upgradeable::UpgradeableComponent::Upgraded',
+        kind: 'nested',
+      },
+    ],
+  },
+  {
+    type: 'event',
+    name: 'hidemi::Hidemi::Event',
+    kind: 'enum',
+    variants: [
+      {
+        name: 'OwnableEvent',
+        type: 'openzeppelin_access::ownable::ownable::OwnableComponent::Event',
+        kind: 'flat',
+      },
+      {
+        name: 'UpgradeableEvent',
+        type: 'openzeppelin_upgrades::upgradeable::UpgradeableComponent::Event',
+        kind: 'flat',
       },
     ],
   },
